a6f8498ad2b588a4fb99069185ab293074ab1844
Piyush Kumar Pandey <piyush.pandey@practo.com>
2025-03-18 13:11:09 +0530

    chore: added reset script

--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -1,14 +1,6 @@
 import type { Preview } from '@storybook/react';
 import { styled } from '@storybook/theming';
 
-// Add global styles for React Native for Web
-const GlobalStyle = styled.div`
-  font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
-  margin: 0;
-  padding: 0;
-`;
-
-
 const preview: Preview = {
   parameters: {
     controls: {
@@ -18,7 +10,8 @@ const preview: Preview = {
       },
     },
     layout: 'centered'
-  }
+  },
+  tags: ['autodocs']
 };
 
 export default preview;
