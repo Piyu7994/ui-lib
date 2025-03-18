05f4d82890a96fc16bf2a4e4a5ec780e84053c52
Piyush Kumar Pandey <piyush.pandey@practo.com>
2025-03-18 16:36:34 +0530

    chore: updated version to 0.2.3

--- src/index.tsx
+++ src/index.tsx
@@ -1,30 +1,3 @@
-import {
-  requireNativeComponent,
-  UIManager,
-  Platform,
-  type ViewStyle,
-} from 'react-native';
-
-const LINKING_ERROR =
-  `The package 'react-native-ui-library' doesn't seem to be linked. Make sure: \n\n` +
-  Platform.select({ ios: "- You have run 'pod install'\n", default: '' }) +
-  '- You rebuilt the app after installing the package\n' +
-  '- You are not using Expo Go\n';
-
-type UiLibraryProps = {
-  color: string;
-  style: ViewStyle;
-};
-
-const ComponentName = 'UiLibraryView';
-
-export const UiLibraryView =
-  UIManager.getViewManagerConfig(ComponentName) != null
-    ? requireNativeComponent<UiLibraryProps>(ComponentName)
-    : () => {
-        throw new Error(LINKING_ERROR);
-      };
-
 export { PtButton } from './components/atoms/Button';
 export { PtText } from './components/atoms/Text';
 export { PtTextInput } from './components/atoms/TextInput';
